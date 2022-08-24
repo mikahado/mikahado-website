d9063819b80caefb7160e22a8c649134ae1d0f97
Michael <mikahado@gmail.com>
2022-08-24 16:37:05 -0400

    total update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This cnotent added by JavaScript";
+/* const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
 
